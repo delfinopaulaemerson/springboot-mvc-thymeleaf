f1d4ed67b9f8ca514910817cfb40685af0a8d83b
delfinopaulaemerson <delfs@hotmail.com>
2019-10-04 18:13:27 -0300

    Springboot MVC Thymeleaf

---
+++ src/main/java/com/boot/mvc/domain/Endereco.java
@@ -0,0 +1,33 @@
+package com.boot.mvc.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "ENDERECOS")
+public class Endereco extends AbstractEntity<Long>{
+	
+	@Column(nullable = false)
+	private String logradouro;
+	
+	@Column(nullable = false)
+	private String bairro;
+	
+	@Column(nullable = false)
+	private String cidade;
+	
+	@Column(nullable = false, length = 2)
+	@Enumerated(EnumType.STRING)
+	private UF uf;
+	
+	@Column(nullable = false, length = 9)
+	private String cep;
+	
+	@Column(nullable = false, length = 5)
+	private Integer numero;
+
+}
